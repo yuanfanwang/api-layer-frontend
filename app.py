5f9836ebc1f697939f68d69229f8a6042fd04563
yuanfanwang <wang.far.sail@gmail.com>
2024-06-23 15:27:35 +0900

    api-layer

--- app.py
+++ app.py
@@ -29,4 +29,4 @@ def buy_product():
     return jsonify({'payment': payment_response.json(), 'purchase': purchase_response.json()})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
